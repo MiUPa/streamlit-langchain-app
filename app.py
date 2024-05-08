f24449acc7cba571363c78b7e5858cf51468cbb0
Miura <miura.mc23@gmail.com>
2024-05-08 09:40:56 +0900

    6.5

--- app.py
+++ app.py
@@ -3,4 +3,11 @@ import streamlit as st
 st.title("langchain-streamlit-app")
 
 prompt = st.chat_input("What is up?")
-print(prompt)
+
+if prompt:
+    with st.chat_message("user"):
+        st.markdown(prompt)
+
+    with st.chat_message("assistant"):
+        response = "こんにちは"
+        st.markdown(response)
